1c43e5810c4b31908f55fa557ee6c9a9eb972de0
dhaval-pandya <mailtodhaval@gmail.com>
2020-01-15 22:27:47 +0530

    adding support to qualitygates

--- sonarqube_api.py
+++ sonarqube_api.py
@@ -38,6 +38,28 @@ def copy_profile(profile_id, profile_name):
 	    raise ApiError('POST /api/qualityprofiles/copy {}'.format(response.status_code))
 	print('Copied Profile from : {} to {}'.format(profile_id,response.json()["name"]))
 	return response
+def create_quality_profile(profile_name,language):
+	url = DEFAULT_HOST + ':' + DEFAULT_PORT+ '/api/qualitygates/create'
+	data  = {'name':profile_name}
+	response = requests.post(url, data=data, auth=('admin', 'admin'))
+
+	##resp = requests.get('http://localhost:9000/api/qualitygates/list')
+	if response.status_code != 200:
+	    # This means something went wrong.
+	    raise ApiError('POST /api/qualitygates/create/ {}'.format(response.status_code))
+	print('Created Profile. ID: {}'.format(response.json()["profile"]["key"]))
+	return response
+def copy_quality_profile(profile_id, profile_name):
+	url = DEFAULT_HOST + ':' + DEFAULT_PORT+ '/api/qualitygates/copy'
+	data  = {'id':profile_id,'name':profile_name}
+	response = requests.post(url, data=data, auth=('admin', 'admin'))
+
+	if response.status_code != 200:
+	    # This means something went wrong.
+	    raise ApiError('POST /api/qualitygates/copy {}'.format(response.status_code))
+	print('Created Profile. ID: {}'.format(response.json()["profile"]["key"]))
+	api/qualitygates/copy
+
 def create_group(groupname):
 	url = DEFAULT_HOST + ':' + DEFAULT_PORT+ '/api/user_groups/create'
 	data  = { 'name': groupname}
@@ -64,12 +86,21 @@ profile_language = os.getenv("profile_language")
 group_name = os.getenv("group_name")
 user_name = os.getenv("user_name")
 copy_profile_id = os.getenv("copy_profile_id")
+quality_copy_profile_id = os.getenv("quality_copy_profile_id")
+quality_profile_name = os.getenv("quality_profile_name")
+
 
 if scenario_choice == 'Create Profile':
 	profile_details = create_profile(profile_name,profile_language)
 	print("Profile created with details " + str(profile_details.json()))
 elif scenario_choice == 'Copy Profile':
 	profile_details = copy_profile(copy_profile_id,profile_name)
+	print("Profile created with details " + str(profile_details.json()))
+elif scenario_choice == 'Create Qualitygates Profile':
+	profile_details = create_quality_profile(quality_profile_name)
+	print("Quality Profile created with details " + str(profile_details.json()))
+elif scenario_choice == 'Copy Qualitygates Profile':
+	profile_details = copy_quality_profile(quality_copy_profile_id,quality_profile_name)
 	print("Profile created with details " + str(profile_details.json()))
 elif scenario_choice == 'Create Profile and Create Group':
 	profile_details = create_profile(profile_name,profile_language)
